916dcd0acc1b731bdc5b6a27e1ff838362d6ef3f
resei <lowrain@live.com>
2017-04-27 01:28:57 +0800

    Update contact_me.php

--- portfolio/mail/contact_me.php
+++ portfolio/mail/contact_me.php
@@ -16,7 +16,7 @@ $mobile = strip_tags(htmlspecialchars($_POST['mobile']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'lowrain@live.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'patricia_gison@yahoo.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMobile: $mobile\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
